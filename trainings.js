539d68af2c71cccfc3720d153d55a92a03a8909a
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-08-21 14:48:48 +0330

    ng-repeat / n in names

--- trainings.js
+++ trainings.js
@@ -1,4 +1,13 @@
+// function MyController($scope) {
+//   $scope.name = "Pourya";
+//   $scope.family = "Soleimani";
+// }
+
 function MyController($scope) {
-  $scope.name = "Pourya";
-  $scope.family = "Soleimani";
+  $scope.people = [
+    { id: 1, firstname: "pourya", lastname: "soleimani", age: 32 },
+    { id: 2, firstname: "mamad", lastname: "boland", age: 33 },
+    { id: 3, firstname: "majid", lastname: "kalhor", age: 31 },
+    { id: 4, firstname: "ehsan", lastname: "gholipoor", age: 31 },
+  ];
 }
