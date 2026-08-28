94f6b791d362f168f549ce81fcbb16e76f8c3cd1
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-08-28 14:32:26 +0330

    REVIEW___ANGULAR__JS

--- trainings.js
+++ trainings.js
@@ -1,23 +1,23 @@
-// function MyController($scope) {
-//   $scope.name = "Pourya";
-//   $scope.family = "Soleimani";
-// }
-
 function MyController($scope) {
-  $scope.people = [
-    { id: 1, firstname: "pourya", lastname: "soleimani", age: 32 },
-    { id: 2, firstname: "mamad", lastname: "boland", age: 33 },
-    { id: 3, firstname: "majid", lastname: "kalhor", age: 31 },
-    { id: 4, firstname: "ehsan", lastname: "gholipoor", age: 31 },
-    { id: 5, firstname: "hamed", lastname: "mousavi", age: 26 },
-    { id: 6, firstname: "ali", lastname: "ghazanfari", age: 25 },
-    { id: 7, firstname: "ali", lastname: "mohammad", age: 25 },
-    { id: 8, firstname: "mohammad", lastname: "yousefi", age: 24 },
-    { id: 9, firstname: "ashkan", lastname: "hoseinpour", age: 26 },
-    { id: 10, firstname: "shahriyar", lastname: "reyhani", age: 33 },
-    { id: 11, firstname: "iman", lastname: "allahyari", age: 30 },
-  ];
+  $scope.name = "Pourya";
+  $scope.family = "Soleimani";
 }
+
+// function MyController($scope) {
+//   $scope.people = [
+//     { id: 1, firstname: "pourya", lastname: "soleimani", age: 32 },
+//     { id: 2, firstname: "mamad", lastname: "boland", age: 33 },
+//     { id: 3, firstname: "majid", lastname: "kalhor", age: 31 },
+//     { id: 4, firstname: "ehsan", lastname: "gholipoor", age: 31 },
+//     { id: 5, firstname: "hamed", lastname: "mousavi", age: 26 },
+//     { id: 6, firstname: "ali", lastname: "ghazanfari", age: 25 },
+//     { id: 7, firstname: "ali", lastname: "mohammad", age: 25 },
+//     { id: 8, firstname: "mohammad", lastname: "yousefi", age: 24 },
+//     { id: 9, firstname: "ashkan", lastname: "hoseinpour", age: 26 },
+//     { id: 10, firstname: "shahriyar", lastname: "reyhani", age: 33 },
+//     { id: 11, firstname: "iman", lastname: "allahyari", age: 30 },
+//   ];
+// }
 
 function MyCrtl($scope) {
   $scope.name = "Pourya";
@@ -39,7 +39,7 @@ app.controller("AppController", function ($scope) {
   };
 });
 
-//& SAMPLE PROJECT
+// SAMPLE PROJECT
 var project = angular.module("ProjectModule", []);
 project.controller("ProjectController", function ($scope) {
   $scope.count = 0;
