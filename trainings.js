175686974cf6d017b8af5065bc8cef3e20e09ad5
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-08-28 18:39:18 +0330

    ANGULAR_JS__REVIEW

--- trainings.js
+++ trainings.js
@@ -15,6 +15,20 @@ function MyController($scope) {
     { id: 6, title: 'MACBOOK__PRO' },
   ]
 
+  $scope.people = [
+    { id: 1, firstname: "pourya", lastname: "soleimani", age: 32 },
+    { id: 2, firstname: "mamad", lastname: "boland", age: 33 },
+    { id: 3, firstname: "majid", lastname: "kalhor", age: 31 },
+    { id: 4, firstname: "ehsan", lastname: "gholipoor", age: 31 },
+    { id: 5, firstname: "hamed", lastname: "mousavi", age: 26 },
+    { id: 6, firstname: "ali", lastname: "ghazanfari", age: 25 },
+    { id: 7, firstname: "ali", lastname: "mohammad", age: 25 },
+    { id: 8, firstname: "mohammad", lastname: "yousefi", age: 24 },
+    { id: 9, firstname: "ashkan", lastname: "hoseinpour", age: 26 },
+    { id: 10, firstname: "shahriyar", lastname: "reyhani", age: 33 },
+    { id: 11, firstname: "iman", lastname: "allahyari", age: 30 },
+  ];
+
   $scope.themeToggle = function () {
     const i = document.querySelector("#theme__btn i");
     if (i.classList.contains("ph-moon")) {
@@ -56,22 +70,6 @@ function MyController($scope) {
 
   }
 }
-
-// function MyController($scope) {
-//   $scope.people = [
-//     { id: 1, firstname: "pourya", lastname: "soleimani", age: 32 },
-//     { id: 2, firstname: "mamad", lastname: "boland", age: 33 },
-//     { id: 3, firstname: "majid", lastname: "kalhor", age: 31 },
-//     { id: 4, firstname: "ehsan", lastname: "gholipoor", age: 31 },
-//     { id: 5, firstname: "hamed", lastname: "mousavi", age: 26 },
-//     { id: 6, firstname: "ali", lastname: "ghazanfari", age: 25 },
-//     { id: 7, firstname: "ali", lastname: "mohammad", age: 25 },
-//     { id: 8, firstname: "mohammad", lastname: "yousefi", age: 24 },
-//     { id: 9, firstname: "ashkan", lastname: "hoseinpour", age: 26 },
-//     { id: 10, firstname: "shahriyar", lastname: "reyhani", age: 33 },
-//     { id: 11, firstname: "iman", lastname: "allahyari", age: 30 },
-//   ];
-// }
 
 function MyCrtl($scope) {
   $scope.name = "Pourya";
